2cad24f30581ea18bcef8b7648ad985098e8cb5e
Avery Durrant <avery246813579@gmail.com>
2018-08-29 16:27:29 -0400

    Setting more things up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import App from './App';
+import LandingScreen from './components/LandingScreen';
 import {render} from 'react-dom'
-import './App.css';
+import './style/App.css';
 
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {createStore, combineReducers} from 'redux'
 
 import userReducers from './reducers/user'
+import LoginScreen from "./components/pre/LoginScreen";
+import SignupScreen from "./components/pre/SignupScreen";
 
 let store = createStore(combineReducers({
     user: userReducers
@@ -16,10 +18,19 @@ let store = createStore(combineReducers({
 render(
     <Provider store={store}>
         <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={App}/>
-                {/*<Route path="*" component={FourZeroFour}/>*/}
-            </Switch>
+            <div className="App">
+                <header className="nav">
+                    <Link to={"/"}>Home</Link>
+                    <Link to={"/login"}>Login</Link>
+                    <Link to={"/signup"}>Signup</Link>
+                </header>
+
+                <Switch>
+                    <Route exact path="/" component={LandingScreen}/>
+                    <Route exact path="/login" component={LoginScreen}/>
+                    <Route exact path="/signup" component={SignupScreen}/>
+                </Switch>
+            </div>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
