1acaec96654db528988097e63c909a818fe01dd0
costaleonardo <costaleonardo97@gmail.com>
2018-08-29 21:32:59 -0400

    Update .gitignore.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'; 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import * as Actions from './actions/index'
 
 import {
     withRouter
 } from 'react-router-dom'
 
-function mapStateToProps(state) {
-    return {user: state.user}
-}
+import store from './store';
 
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators(Actions, dispatch)
-}
+import Nav from './components/layout/Nav';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <p className="dog">
-              Hi
-          </p>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Nav />
+            <Switch>
+              {/* <Route exact path="/" component={App}/> */}
+              {/*<Route path="*" component={FourZeroFour}/>*/}
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
+};
+
+function mapStateToProps(state) {
+  return {user: state.user}
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch)
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
